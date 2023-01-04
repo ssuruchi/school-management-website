e927c3606c00f706bf7a37255cbb5cd4fd5c8557
Anurag Singh <111911708+sinferno-1@users.noreply.github.com>
2023-01-04 20:10:35 +0000

    Fixed Bugs

--- accountant/payments.php
+++ accountant/payments.php
@@ -3,7 +3,7 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
+        <!-- <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Home | ERP Model</title>
         <link rel="stylesheet" href="./assets/css/base-styles.css">
@@ -12,7 +12,7 @@
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 
 		<style>
 			input[type=checkbox]
